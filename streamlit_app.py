b3168b78715fdb3c4a86b37b0d82f763e3f6c0bf
ashwinipandey-cmd <ashwanipandey673@gmail.com>
2026-02-19 16:06:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3973,7 +3973,7 @@ else:
         </div>
         """, unsafe_allow_html=True)
 
-    st.markdown("<div style='border-bottom:1px solid rgba(56,189,248,0.12);margin:6px 0 10px'></div>",
+    st.markdown("""<div style='border-bottom:1px solid rgba(56,189,248,0.12);margin:6px 0 10px'></div>""",
                 unsafe_allow_html=True)
 
     # ── Profile panel slides in when avatar circle is clicked ──────────────────
